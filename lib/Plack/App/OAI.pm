ed70314501b2156a2862080b7cb9637afa3efcf4
Emmanuel Di Pretoro <edipretoro@gmail.com>
2013-03-10 23:58:37 +0100

    * lib/Plack/App/OAI.pm: another small increment to pass the '03-psgi.t' tests

--- lib/Plack/App/OAI.pm
+++ lib/Plack/App/OAI.pm
@@ -5,7 +5,7 @@ package Plack::App::OAI;
 use parent 'Plack::Component';
 
 sub call {
-    return [ 200, [ 'Content-Type' => 'text/plain' ], [ 'Hello' ] ];
+    return [ 200, [ 'Content-Type' => 'text/xml' ], [ '<?xml version="1.0" encoding="utf-8"?>' ] ];
 }
 
 1;
