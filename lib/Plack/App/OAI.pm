85745e5c069acf56e4bb6252472619965ba5dad2
Emmanuel Di Pretoro <edipretoro@gmail.com>
2013-03-18 22:28:10 +0100

    * lib/Plack/App/OAI.pm: first increment to pass the tests in '04-oai_verbs.t'

--- lib/Plack/App/OAI.pm
+++ lib/Plack/App/OAI.pm
@@ -3,9 +3,18 @@ use warnings;
 package Plack::App::OAI;
 
 use parent 'Plack::Component';
+use Plack::Request;
 
 sub call {
-    return [ 200, [ 'Content-Type' => 'text/xml' ], [ '<?xml version="1.0" encoding="utf-8"?>' ] ];
+    my ( $self, $env ) = @_;
+    my $req = Plack::Request->new( $env );
+
+    my $verb = $req->param('verb') || '';
+    return
+        [ 200,
+          [ 'Content-Type' => 'text/xml' ],
+          [ '<?xml version="1.0" encoding="utf-8"?>' . "\n" . '<OAI-PMH>' . $verb . '</OAI->can-PMH>' ]
+      ];
 }
 
 1;
